ff6a2b3c80bc93ad1ef17fedfd0cd73cfd97e520
KaueMiziara <kauemiziara@gmail.com>
2023-02-10 11:07:51 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Enigma Simulator (CLI)
+Encrypt messages using the Enigma machine (CLI version)
---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+// Path: Keyboad input -> Plugboard -> Rotors -> Plugboard -> Output
+
+// First version, hard input the following settings:
+// Reflector: A
+// Rotors: I-II-III
+// Plugboard: A-B, C-D, E-F
+// Message: A => Q
+
+// Reflector A: EJMZALYXVBWFCRQUONTSPIKHGD
+// Rotor I:     EKMFLGDQVZNTOWYHXUSPAIBRCJ, notch: Q
+// Rotor II:    AJDKSIRUXBLHWTMCQGZNPYFVOE, notch: E
+// Rotor III:   BDFHJLCPRTXVZNYEIWGAKMUSQO, notch: V
+
+#include <iostream>
+
+int main(int argc, char **argv) {
+    std::cout << "Hello, world!" << std::endl;
+}
